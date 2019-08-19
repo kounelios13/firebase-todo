573d8953e7450fc3c61d4140905e20dbccfeabae
Manos Kounelakis <pkounelios@gmail.com>
2019-08-19 12:32:57 +0300

    add firebase cinfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC6PlP_qYN8JLO_KixwX1V7QgsAuPDfEZk",
+    authDomain: "todo2-f4442.firebaseapp.com",
+    databaseURL: "https://todo2-f4442.firebaseio.com",
+    projectId: "todo2-f4442",
+    storageBucket: "",
+    messagingSenderId: "524168977680",
+    appId: "1:524168977680:web:677200315faa5f47"
+  }
 };
 
 /*
